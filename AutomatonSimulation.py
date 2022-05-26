949b07e1c251ed62e27551538bab0e461aac79da
Adam <adam.p.kulczycki@gmail.com>
2022-05-26 11:17:52 +0200

    Merged temperature view into branch

--- AutomatonSimulation.py
+++ AutomatonSimulation.py
@@ -4,7 +4,15 @@ from BlocksEnvironment import BlocksEnvironment
 class AutomatonSimulation:
     def __init__(self, block_environment: BlocksEnvironment):
         self.block_environment = block_environment
-        #TODO: add neighbours here
+        for x in range(self.block_environment.size[0]):
+            for y in range(self.block_environment.size[1]):
+                for z in range(self.block_environment.size[2]):
+                    for i in [-1, 1]:
+                        pass
+                    for j in [-1, 1]:
+                        pass
+                    for k in [-1, 1]:
+                        pass
 
     def next_step(self):
         for x in range(self.block_environment.size[0]):
