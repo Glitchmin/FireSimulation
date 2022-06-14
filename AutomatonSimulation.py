e765f9940b978840630af278df7846d2cae83652
Adam <adam.p.kulczycki@gmail.com>
2022-06-14 11:40:21 +0200

    added radiation neighbours

--- AutomatonSimulation.py
+++ AutomatonSimulation.py
@@ -1,8 +1,10 @@
 import threading
 from copy import copy
 
+from ursina import *
 from BlocksEnvironment import BlocksEnvironment
 from Cell import Cell
+import sys
 
 
 class AutomatonSimulation(threading.Thread):
@@ -12,6 +14,12 @@ class AutomatonSimulation(threading.Thread):
         self.s_for_every_step = 1
         self.block_environment.cells[0][0][0].state.temperature = 1200
         self.block_environment.cells[0][0][0].next_state.temperature = 1200
+        for x in range(self.block_environment.size[0]):
+            for y in range(self.block_environment.size[1]):
+                for z in range(self.block_environment.size[2]):
+                    cell: Cell = self.block_environment.cells[x][y][z]
+                    if cell.material_properties.is_gas():
+                        cell.collider = None
         for x in range(self.block_environment.size[0]):
             for y in range(self.block_environment.size[1]):
                 for z in range(self.block_environment.size[2]):
@@ -28,10 +36,60 @@ class AutomatonSimulation(threading.Thread):
                             if 0 <= z + k < self.block_environment.size[2]:
                                 self.block_environment.cells[x][y][z].add_neighbor(
                                     self.block_environment.cells[x][y][z + k])
+
+                    cell: Cell = self.block_environment.cells[x][y][z]
+                    cell.collider = None
+                    if cell.voxel is not None:
+                        cell.voxel.collider = None
+                    if not cell.material_properties.is_gas():
+                        print(x, y, z)
+                        for x2 in [0, self.block_environment.size[0] - 1]:
+                            for y2 in range(max(0, y - 5), min(self.block_environment.size[1], y + 5)):
+                                for z2 in range(max(0, z - 5), min(self.block_environment.size[2], z + 5)):
+                                    cell2: Cell = self.block_environment.cells[x2][y2][z2]
+                                    self.dispatch_ray(cell, cell2)
+                        for x2 in range(max(0, x - 5), min(self.block_environment.size[0], x + 5)):
+                            for y2 in [0, self.block_environment.size[1] - 1]:
+                                for z2 in range(max(0, z - 5), min(self.block_environment.size[2], z + 5)):
+                                    cell2: Cell = self.block_environment.cells[x2][y2][z2]
+                                    self.dispatch_ray(cell, cell2)
+                        for x2 in range(max(0, x - 5), min(self.block_environment.size[0], x + 5)):
+                            for y2 in range(max(0, y - 5), min(self.block_environment.size[1], y + 5)):
+                                for z2 in [0, self.block_environment.size[2] - 1]:
+                                    cell2: Cell = self.block_environment.cells[x2][y2][z2]
+                                    self.dispatch_ray(cell, cell2)
+                    cell.collider = BoxCollider(cell, center=Vec3(cell.position[0] + 0.5, cell.position[1] + 0.5,
+                                                                  cell.position[2] + 0.5), size=Vec3(1, 1, 1))
+                    if cell.voxel is not None:
+                        cell.voxel.collider = BoxCollider(cell,
+                                                          center=Vec3(cell.position[0] + 0.5, cell.position[1] + 0.5,
+                                                                      cell.position[2] + 0.5), size=Vec3(1, 1, 1))
+
+        with open('neighbours.txt', 'w') as f:
+            original_stdout = sys.stdout
+            sys.stdout = f  # Change the standard output to the file we created.
+            for x in range(self.block_environment.size[0]):
+                for y in range(self.block_environment.size[1]):
+                    for z in range(self.block_environment.size[2]):
+                        self.block_environment.cells[x][y][z].radiation_neighors = \
+                        list(set(self.block_environment.cells[x][y][z].radiation_neighbors))
+                        print("(", x, y, z, ")", list(set(self.block_environment.cells[x][y][z].radiation_neighbors)))
+            sys.stdout = original_stdout
+
+    def dispatch_ray(self, cell, cell2):
+        hit_info = raycast(cell.position, cell2.position - cell.position, distance=5)
+        if hit_info.hit:
+            cell_hit: Cell = \
+                self.block_environment.cells[int(hit_info.entity.position[0])][int(hit_info.entity.position[1])][
+                    int(hit_info.entity.position[2])]
+            if not cell_hit.material_properties.is_gas():
+                cell_hit.radiation_neighbors.append(cell.position)
+                cell.radiation_neighbors.append(cell_hit.position)
+
     def next_10(self):
         self.next_step(100)
 
-    def next_step(self, n = 1):
+    def next_step(self, n=1):
         for i in range(n):
             for x in range(self.block_environment.size[0]):
                 for y in range(self.block_environment.size[1]):
--- main.py
+++ main.py
@@ -10,13 +10,14 @@ window.borderless = False
 # window.fullscreen = True
 window.exit_button.visible = True
 window.fps_counter.enabled = True
-env = BlocksEnvironment(15, 15, 15)
+env = BlocksEnvironment(5, 5, 5)
 sim = None
 sim_button = None
 sim_tooltip = None
 
 def start_sim():
     global sim, sim_button
+    sim = AutomatonSimulation(env)
     sim.start()
     # sim_button.text = 'next step'
     # sim_button.on_click = sim.next_step
@@ -25,7 +26,6 @@ def start_sim():
 if __name__ == "__main__":
     # def update():
 
-    sim = AutomatonSimulation(env)
     b = Button(text='view \nmode', color=color.azure, scale=.1, text_origin=(-.5, 0),
                position=window.bottom_left + (0.07, 0.07))
     b.on_click = env.switch_view_mode
@@ -36,7 +36,7 @@ if __name__ == "__main__":
 
     next_button = Button(text='next 10', color=color.azure, scale=(0.18, 0.1), text_origin=(-.5, 0),
                         position=window.bottom_left + (0.5, 0.07))
-    next_button.on_click = sim.next_step
+    #next_button.on_click = sim.next_step
 
 
     player = EditorCamera()
